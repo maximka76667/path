f994af4a2e1b0161f5c732e0ca0a0a01d96d3482
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-04 13:25:19 +0100

    Initial commit

---
+++ src/path/Main.java
@@ -0,0 +1,28 @@
+package path;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Path path = Paths.get("C:\\Users\\Usuario\\Documents");
+
+		// Main methods
+		System.out.println("Path: " + path);
+		System.out.println("Requesting file name: " + path.getFileName());
+		System.out.println("First file on the path: " + path.getName(0));
+		System.out.println("Root element: " + path.getRoot());
+		System.out.println("Count of files on the path: " + path.getNameCount());
+		System.out.println("Parent of requesting file: " + path.getParent());
+
+		// Resolve (add path to the end)
+		Path resolvedPath = path.resolve("fichero.txt");
+		System.out.println(resolvedPath);
+
+		// System path
+		Path systemPath = Paths.get(System.getProperty("user.dir"));
+		System.out.println(systemPath);
+	}
+
+}
